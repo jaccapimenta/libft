9c5aa328ce69bcbe6953b2bf65f4f4c0ca73bcd6
Jacqueline Pimenta <jaccapimenta@gmail.com>
2026-07-01 13:24:31 -0300

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -36,40 +35,3 @@ size_t	strlcat(char *dst, const char *src, size_t dstsize)
 	dst[dst_len + i] = '\0';
 	return (dst_len + src_len);
 }
-=======
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jacavalc <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/10 15:31:01 by jacavalc          #+#    #+#             */
-/*   Updated: 2026/06/10 16:11:06 by jacavalc         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
-
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
-{
-	size_t	i;
-	size_t	len_dst;
-	size_t	len_src;
-
-	i = 0;
-	len_dst = ft_strlen(dst);
-	len_src = ft_strlen(src);
-	if (len_dst >= size)
-		return (size + len_src);
-	while (len_dst + 1 < size && src[i])
-	{
-		dst[len_dst] = src[i];
-		i++;
-		len_dst++;
-	}
-	len_src -= i;
-	dst[len_dst] = '\0';
-	return (len_dst + len_src);
-}
->>>>>>> e75cd28 (new)
