6b086dd71f2aa53b4e01e14e960d167c95fc9fde
Jacqueline Pimenta <jaccapimenta@gmail.com>
2026-07-01 13:27:36 -0300

    Update libft.h

--- libft.h
+++ libft.h
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jacavalc <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/01 15:14:38 by jacavalc          #+#    #+#             */
-/*   Updated: 2026/06/22 16:14:18 by jacavalc         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <unistd.h>
-# include <stdlib.h>
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-
-size_t	ft_strlen(const char *s);
-size_t	strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	strlcat(char *dst, const char *src, size_t dstsize);
-
-int		ft_strncmp(char *s1, char *s2, unsigned int n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-int		ft_atoi(const char *nptr);
-
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-
-void	*ft_calloc(size_t nmemb, size_t size);
-char	*ft_strdup(const char *s);
-
-=======
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -90,5 +43,4 @@ char	*ft_strrchr(const char *s, int c);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 
->>>>>>> e75cd28 (new)
 #endif
