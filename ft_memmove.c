b05c91d7ed86b10c90a0d1fc69f858f2f4f17977
Jacqueline Pimenta <jaccapimenta@gmail.com>
2026-07-01 13:22:24 -0300

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jacavalc <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/10 15:31:47 by jacavalc          #+#    #+#             */
-/*   Updated: 2026/06/22 15:57:31 by jacavalc         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
-
-void	*ft_memmove(void *dest, const void *src, size_t n)
-{
-	size_t			i;
-	unsigned char	*pdest;
-	unsigned char	*psrc;
-
-	if (!dest && !src)
-		return (NULL);
-	pdest = (unsigned char *)dest;
-	psrc = (unsigned char *)src;
-	if (dest > src)
-		while (n--)
-			pdest[n] = psrc[n];
-	else
-	{
-		i = 0;
-		while (i < n)
-		{
-			pdest[i] = psrc[i];
-			i++;
-		}
-	}
-	return (dest);
-}
-=======
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -79,4 +39,3 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
->>>>>>> e75cd28 (new)
