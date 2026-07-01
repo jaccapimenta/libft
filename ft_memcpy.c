19d6465938413931f2da4c2c9ae1671f89c3f923
Jacqueline Pimenta <jaccapimenta@gmail.com>
2026-07-01 13:21:57 -0300

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jacavalc <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/10 15:31:44 by jacavalc          #+#    #+#             */
-/*   Updated: 2026/06/22 15:59:25 by jacavalc         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
-
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	size_t			i;
-	unsigned char	*pdest;
-	unsigned char	*psrc;
-
-	if (!dest && !src)
-		return (NULL);
-	pdest = (unsigned char *)dest;
-	psrc = (unsigned char *)src;
-	i = 0;
-	while (i < n)
-	{
-		pdest[i] = psrc[i];
-		i++;
-	}
-	return (dest);
-}
-=======
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -64,4 +30,3 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
->>>>>>> e75cd28 (new)
