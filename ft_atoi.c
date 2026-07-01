8d8e54d691630b78ea8ffc453596dba3815a4fb5
Jacqueline Pimenta <jaccapimenta@gmail.com>
2026-07-01 13:18:01 -0300

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jacavalc <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/28 17:50:57 by jacavalc          #+#    #+#             */
-/*   Updated: 2026/06/22 16:18:03 by jacavalc         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
-
-int	ft_atoi(const char *nptr)
-{
-	int	negative;
-	int	number;
-
-	negative = 1;
-	number = 0;
-	while (*nptr == ' ' || (*nptr >= 9 && *nptr <= 13))
-		nptr++;
-	while (*nptr == '-' || *nptr == '+')
-	{
-		if (*nptr == '-')
-			negative = -1;
-		nptr++;
-	}
-	while (*nptr >= '0' && *nptr <= '9')
-	{
-		number = number * 10 + (*nptr - '0');
-		nptr++;
-	}
-	return (number * negative);
-}
-
-// int	main(void)
-// {
-// 	char	str[] = " -12345ab56";
-
-// 	printf("%d", ft_atoi(str));
-// 	return (0);
-// }
-=======
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -82,4 +36,3 @@ int	ft_atoi(const char *nptr)
 	}
 	return (sign * number);
 }
->>>>>>> e75cd28 (new)
